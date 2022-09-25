3224c8b28db3924f8ce42681ead96bef47b02733
JustinLien0805 <6998dragoan@gmail.com>
2022-09-25 20:09:46 +0800

    Initial Commit

---
+++ swift-chart/swift_chartApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_chartApp.swift
+//  swift-chart
+//
+//  Created by 練家辰 on 2022/9/25.
+//
+
+import SwiftUI
+
+@main
+struct swift_chartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
